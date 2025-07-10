cae699729979427d7a5d53ba249d63bb559bc11c
Kenchin33 <169151335+Kenchin33@users.noreply.github.com>
2025-07-10 19:11:28 +0300

    import useref

--- frontend/src/pages/CrashGame.js
+++ frontend/src/pages/CrashGame.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { FaUserCircle } from 'react-icons/fa';
 import axios from 'axios';
