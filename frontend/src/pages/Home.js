d1449fadb1da4c669ae8f5d4cee263a11e155df1
Kenchin33 <169151335+Kenchin33@users.noreply.github.com>
2025-07-11 18:40:33 +0300

    fix

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -108,21 +108,24 @@ const Home = () => {
             );
           })
         )}
-              <div className="cases-list">
-                  {filtered.map(c => (
-                    <Link to="/crash" className="case-card" style={{ cursor: 'pointer' }}>
-                    <img
-                      src="/images/image.jpg"  // Можеш замінити на власне зображення літачка
-                      alt="Грати в Літачка"
-                      className="case-image"
-                      />
-                        <div className="case-info">
-                          <h3>Грати в Літачка</h3>
-                          <p>Спробуй виграти фігурки!</p>
-                        </div>
-                  </Link>
-                  ))}
-                </div>
+
+<section>
+  <h3 className="category-title">Міні-ігри</h3>
+  <div className="cases-list">
+    <Link to="/crash" className="case-card" style={{ cursor: 'pointer' }}>
+      <img
+        src="/images/image.jpg"
+        alt="Грати в Літачка"
+        className="case-image"
+      />
+      <div className="case-info">
+        <h3>Літачок</h3>
+        <p>Зроби свої фігурки ціннішими!</p>
+      </div>
+    </Link>
+  </div>
+</section>
+
       </main>
     </div>
   );
