825fdfc69e6d27c5d802f08423d09cf36b274768
Kenchin33 <169151335+Kenchin33@users.noreply.github.com>
2025-07-12 23:34:48 +0300

    import useRef

--- frontend/src/pages/Exchange.js
+++ frontend/src/pages/Exchange.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FaUserCircle } from 'react-icons/fa';
 import './style.css';
